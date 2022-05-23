30a26f694a0f700b54e6edbd78dfa2007b8aa80c
wes-mitchell <wmdrums@gmail.com>
2022-05-23 14:51:29 -0500

    completes phase four

--- Program.cs
+++ Program.cs
@@ -9,7 +9,8 @@ namespace GuessingGame
         Console.WriteLine("Yo, can you guess the secret number??? You've got four shots.");
         for (int i = 0; i < 4; i++) {
         string response = Console.ReadLine();
-        int SecretNum = 51;
+        Random r = new Random();
+        int SecretNum = r.Next(0, 100);
         if (int.Parse(response) == SecretNum)
         {
           i = 5;
@@ -19,12 +20,12 @@ namespace GuessingGame
         {
           if (i + 2 <= 4) 
           {
-            Console.WriteLine("Bummer, try again. ");
+            Console.WriteLine($"Bummer, try again. Your last guess was ({int.Parse(response)})");
           }  
 
           if (i + 2 < 5) 
           {
-              Console.Write($"Guess Number ({i + 2}) => ");
+            Console.Write($"You've got ({3 - i}) guesses left => ");
           }
         }
     };
