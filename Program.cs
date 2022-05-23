160e07b6a3cc38a39816a291f3425b6ae7bc811a
wes-mitchell <wmdrums@gmail.com>
2022-05-23 13:22:59 -0500

    completes phase one

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Yo, can you guess the secret number???");
+            string response = Console.ReadLine();
+            Console.WriteLine($"You guessed {response}.");
+        }
+    }
+}
+
+
+
+// Display a message to the user asking them to guess the secret number.
+// Display a prompt for the user's guess.
+// Take the user's guess as input and save it as a variable.
+// Display the user's guess back to the screen.
